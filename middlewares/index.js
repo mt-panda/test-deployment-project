1e0a93d7dfad15cec4d7f1f9417985ff4ac12819
tahirrafiqg <tahirrafiqg@gmail.com>
2025-10-03 12:56:52 +0500

    Something about the commit

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,6 +2,16 @@ export const auth = ( req, res, next ) => {
   const existingUser = process.env.USER_NAME
   console.log("Existing User: ", existingUser)
   console.log("Request.body: ", req.body)
+  
+  console.log("Authentication Successful")
+  next();
+  
+  if (!req.body) {
+    return res.status(400).json({
+      message: "Request body is missing"
+    });
+  }
+  
   const { user } = req.body;
   if (!user) {
     return res.status(400).json({
